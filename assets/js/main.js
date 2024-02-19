dbbc135b38651c033d90130087536b87cdbe3933
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-19 16:15:48 +0100

    write exercise text and pseudo code with logic

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* exercise
+
+create a program to calculate the price of a ticket for a train ride.
+The main variables are the km of the ride and the age of the customer, since the enterprise offers a discount based on the age.
+the price for each km is 0.21 €.
+the discount is 20% for minors (17 years old or younger) and 40% for seniors (over 65 years old).
+*/
+
+/* pseudo code
+
+ask the customer for their age and the km of the ride.
+then calculate the final price by multiplying the km for the relative price.
+if the customer is 17 or less years old apply the expected discount,
+else if the customer is 65 years old or older apply the expected discount.
+else show the regular price.
+*/
+
